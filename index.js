3126d4cc2330b8c6df4a15bffd7374040044d1fb
Ernesto Fernandez <cora.ernesto@gmail.com>
2019-01-02 00:20:03 -0300

    api

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = process.env.PORT || 5000
 const job = require(`./app.js`);
 job.run;
 
-//express()
-//  .use(express.static(path.join(__dirname, 'public')))
-//  .set('views', path.join(__dirname, 'views'))
-//  .set('view engine', 'ejs')
-//  .get('/', (req, res) => res.render('pages/index'))
-//  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
